e3b6d05c5bf6ad5c8034a5cd75856ab12076344a
Shahidul Islam <sislam@csumb.edu>
2025-08-25 11:56:54 -0700

    Week1 Day1 Review

--- src/MondayMorning/Wk1S1.java
+++
@@ -1,7 +0,0 @@
-package MondayMorning;
-
-public class Wk1S1 {
-    public static void main(String[] args){
-        System.out.println("Hello");
-    }
-}
---
+++ src/MondayMorning/Wk1S1_Review.java
@@ -0,0 +1,34 @@
+package MondayMorning;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Wk1S1_Review {
+    public static void main(String[] args){
+        int range = 10;
+        int[] data = new int[range];
+        data[0] = 3;
+        data[1] = 5;
+
+        populateArray(data);
+
+        for (int num:data){
+            System.out.println(num);
+        }
+    }
+
+    public static void populateArray(int[] data){
+        Random r = new Random();
+        Scanner in = new Scanner(System.in);
+        for(int i = 0; i < data.length; i++){
+            if(i % 2 == 0) {
+                System.out.print("Enter a number: ");
+                data[i] = in.nextInt();
+//                data[i] = i + 1;
+            }
+            else
+//                data[i] = i * i;
+                data[i] = r.nextInt(100);
+        }
+    }
+}
