9052c59fe9ff3ae2cb79ad97302030d1e0ede632
Shahidul Islam <sislam@csumb.edu>
2025-08-29 13:16:58 -0700

    Week1 Session3 Class

---
+++ src/Section51/Week1/Main.java
@@ -0,0 +1,26 @@
+package Section51.Week1;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner cin = new Scanner(System.in);
+        System.out.print("Enter Filename: ");
+        String fileName = cin.next();
+        loadData(fileName);
+    }
+
+    public static void loadData(String fileName) throws IOException {
+        FileReader fr = new FileReader(fileName);
+        Scanner fin = new Scanner(fr);
+
+        while(fin.hasNextInt()){
+            int num = fin.nextInt();
+            System.out.println(num);
+        }
+
+        fr.close();
+    }
+}
