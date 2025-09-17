0eac5e45d7c7f7199d26861a71d7426e8f813071
Shahidul Islam <sislam@csumb.edu>
2025-09-17 13:10:21 -0700

    Week 4 Wednesday Update 2

---
+++ src/Section52/Week4/QueueRunner.java
@@ -0,0 +1,22 @@
+package Section52.Week4;
+
+public class QueueRunner {
+    public static void main(String[] args){
+        Queue q1 = new Queue();
+        q1.enqueue(12);
+        q1.enqueue(7);
+        q1.enqueue(5);
+        System.out.println("Array: " + q1.arrayState());
+        System.out.println("Queue: " + q1);
+        q1.dequeue();
+        q1.enqueue(2);
+        System.out.println("Array: " + q1.arrayState());
+        System.out.println("Queue: " + q1);
+        q1.dequeue();
+        q1.enqueue(9);
+        q1.enqueue(3);
+        System.out.println("Array: " + q1.arrayState());
+        System.out.println("Queue: " + q1);
+    }
+
+}
