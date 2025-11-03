02d1626e4756fe4a6df9e38aec27a4f1673d573f
Shahidul Islam <sislam@csumb.edu>
2025-11-03 11:03:37 -0800

    Week 11 Monday Update 1

---
+++ src/MondayMorning/Week11/LinkedList.java
@@ -0,0 +1,14 @@
+package MondayMorning.Week11;
+
+public class LinkedList {
+    private Node first;
+}
+
+class Node{
+    //Payload
+    private int data;
+    //private String test;
+
+    //Reference to the next node in the Linked List
+    private Node next;
+}
